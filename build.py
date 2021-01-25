6dfba82e33df34aeaf31e99256793e614e16d150
Greg P <gplechaty@gmail.com>
2021-01-24 20:56:42 -0700

    modified build.py so that the title in htmls change for each page. used safe substitute from template class

--- build.py
+++ build.py
@@ -1,15 +1,34 @@
 ### Site Generator created  1/24 ###
 
-top = open("./templates/top.html").read()
+#top = open("./templates/top.html").read()
 bottom = open("./templates/bottom.html").read()
 index = open("./content/index.html").read()
 about = open("./content/about.html").read()
 projects = open("./content/projects.html").read()
 thoughts = open("./content/thoughts.html").read()
 
+
+#trying variables
+from string import Template
+
+#index
+top = Template(open("./templates/top.html").read())
+top = top.safe_substitute(title='')
 index_full = top + index + bottom
+
+#about
+top = Template(open("./templates/top.html").read())
+top = top.safe_substitute(title=' - About')
 about_full = top + about + bottom
+
+#projects
+top = Template(open("./templates/top.html").read())
+top = top.safe_substitute(title=' - Projects')
 projects_full = top + projects + bottom
+
+#thoughts
+top = Template(open("./templates/top.html").read())
+top = top.safe_substitute(title=' - Thoughts')
 thoughts_full = top + thoughts + bottom
 
 open("./docs/index.html", "w+").write(index_full)
