e1d5ce910fb5d4a3000d4d2547ad620e54cc59ed
Greg P <gplechaty@gmail.com>
2021-02-15 21:45:42 -0700

    -build.py split into manage and utils files. -manage.py takes two arguments: build (create site), and new (creates new content file).

--- utils.py
+++ utils.py
@@ -1,12 +1,30 @@
 ### Site Generator created  1/24 ###
 ### refactored 2/3/21 ###
-#from string import Template
+### jinja2 added 2/13/21###
 import datetime
 import glob
 import os
 from jinja2 import Template
+##########################################################
 
+### Create dictionaries - pages and blog_posts
 
+def create_page_list():
+    all_html_files = glob.glob("content/*.html")
+    pages = []
+    for file in all_html_files:
+        filename = os.path.basename(file)
+        name_only, extension = os.path.splitext(filename)
+        title = name_only.capitalize()
+        content = "./content/" + filename
+        output = "./docs/" + filename
+        pages.append({
+            "title": title,
+            "filename": content,
+            "output": output,
+            "link": filename,
+        })
+    return pages
 
 blog_posts = [
     {
@@ -35,23 +53,13 @@ blog_posts = [
     },
 ]
 
+### Create Blank html page (in content directory)
 
-### Create html page list
-def create_page_list():
-    all_html_files = glob.glob("content/*.html")
-    pages = []
-    for file in all_html_files:
-        filename = os.path.basename(file)
-        name_only, extension = os.path.splitext(filename)
-        title = name_only.capitalize()
-        content = "./content/" + filename
-        output = "./docs/" + filename
-        pages.append({
-            "title": title,
-            "filename": content,
-            "output": output,
-        })
-    return pages
+def create_new_html_page(file_name_input):
+    #file_name scrubbed
+    content_template_html = """<div class="row"></div>"""
+    open("./content/" + file_name_input, "w+").write(content_template_html)
+    return file_name_input
 
 
 ### Read input files
@@ -59,53 +67,22 @@ def create_page_list():
 def read_content(file_name):
     return open(file_name).read()
 
-### Placeholder replacement
+### Placeholder replacement, using Jinja
 
-
-def placeholder_replacement_base(base,page_title,content):
+def placeholder_replacement_base(base,page_title,content,pages):
     base = Template(base)
-    #navbar
-    nav_index=''
-    nav_thoughts=''
-    nav_projects=''
-    nav_about=''
-    if page_title == 'Index':
-        nav_index=' active'
-    elif page_title == 'Thoughts':
-        nav_thoughts=' active'
-    elif page_title == 'Projects':
-        nav_projects=' active'
-    elif page_title == 'About':
-        nav_about=' active'
- 
-    return base.render(nav_index=nav_index,
-                                nav_thoughts=nav_thoughts,
-                                nav_projects=nav_projects,
-                                nav_about=nav_about,
-                                content=content,
-                                title=page_title,
-                                get_year=datetime.datetime.now().strftime("%Y")
-                                )
+    return base.render({
+            'pages_dict': pages,
+            'content': content,
+            'title': page_title,
+            'get_year': datetime.datetime.now().strftime("%Y")
+})
 
 
-def replace_placeholders(file,content,page_title=''):
-    base_template = Template(file)
-    return base_template.render(content=content,title=page_title,get_year=datetime.datetime.now().strftime("%Y"))
-
-def replace_blog_placeholders(blog_base,blog_post_title,blog_post_subtitle,blog_post_image,blog_post_content=''):
-    blog_base_template = Template(blog_base)
-    return blog_base_template.render(blog_post_title=blog_post_title,blog_post_subtitle=blog_post_subtitle,blog_post_image=blog_post_image,blog_post_content=blog_post_content)
+### write 'thoughts' blog pages
 
 
-### write final pages
-
-def write_pages(complete_page,filename):
-    open(filename, "w+").write(complete_page)
-
-
-
-
-def write_blog_posts(blog_posts,base):
+def write_blog_posts(blog_posts,base,pages):
     for post in blog_posts:
         #define variables
         blog_post_date = post['date']
@@ -113,33 +90,26 @@ def write_blog_posts(blog_posts,base):
         blog_post_subtitle = post['subtitle']
         blog_post_output = post['output']
         blog_post_image = post['image']
-        nav_index=''
-        nav_thoughts=' active'
-        nav_projects=''
-        nav_about=''
         #read input files
         blog_base = open("./templates/blog_base.html").read()
         blog_post_content = open(post['filename']).read()
         #set variable text
         #Write blog
         blog_base_template = Template(blog_base)
-        blog_base_final = blog_base_template.render(
-            blog_post_image = blog_post_image,
-            blog_post_title = blog_post_title,
-            blog_post_subtitle = blog_post_subtitle,
-            blog_post_content = blog_post_content,
-        )
+        blog_base_final = blog_base_template.render({
+            'blog_post_image': blog_post_image,
+            'blog_post_title': blog_post_title,
+            'blog_post_subtitle': blog_post_subtitle,
+            'blog_post_content': blog_post_content,
+        })
         #write complete blog page
         base_template = Template(base)
-        blog_page_final = base_template.render(
-                                title=' - Thoughts',
-                                nav_index = nav_index,
-                                nav_thoughts=nav_thoughts,
-                                nav_projects=nav_projects,
-                                nav_about=nav_about,
-                                content=blog_base_final,
-                                get_year=datetime.datetime.now().strftime("%Y")
-        )
+        blog_page_final = base_template.render({
+                                'title': 'Thoughts',
+                                'pages_dict': pages,
+                                'content': blog_base_final,
+                                'get_year': datetime.datetime.now().strftime("%Y"),
+        })
         write_pages(blog_page_final,post['output'])
 
 def write_thoughts_blog_past_posts(blog_posts_info,past_posts_html):
@@ -156,13 +126,13 @@ def write_thoughts_blog_past_posts(blog_posts_info,past_posts_html):
         past_post_layout = open(past_posts_html).read()
         #set variable text
         past_post_layout_template = Template(past_post_layout)
-        past_post_layout_template_with_subs = past_post_layout_template.render(
-                            blog_post_link=blog_post_output,
-                            blog_post_title=blog_post_title,
-                            blog_post_date=blog_post_date,
-                            blog_post_subtitle=blog_post_subtitle,
-                            blog_post_image=blog_post_image,
-                            )
+        past_post_layout_template_with_subs = past_post_layout_template.render({
+                            'blog_post_link': blog_post_output,
+                            'blog_post_title': blog_post_title,
+                            'blog_post_date': blog_post_date,
+                            'blog_post_subtitle': blog_post_subtitle,
+                            'blog_post_image': blog_post_image,
+        })
         blog_past_posts = blog_past_posts + past_post_layout_template_with_subs
     return blog_past_posts
     
@@ -182,32 +152,38 @@ def write_thoughts_content(thoughts_base,blog_posts,blog_past_posts):
                 blog_past_posts = blog_past_posts,
             )
 
+
+### write final pages
+
+
+def write_pages(complete_page,filename):
+    open(filename, "w+").write(complete_page)
+
+
+
+###########################################################################################
 def main():
     pages = create_page_list()
     for page in pages:
         #define variables
         file_name = page['filename']
         file_output = page['output']
-        file_title = ' - ' + page['title']
+        file_title = page['title']
         #read input files
         base = open("./templates/base.html").read()
         base_html = read_content(file_name)
-        
         if page['title'] == 'Thoughts':
             #write specific blog post
-            write_blog_posts(blog_posts,base)
+            write_blog_posts(blog_posts,base,pages)
             #write thoughts - first past posts, then complete page
             blog_past_posts = write_thoughts_blog_past_posts(blog_posts,"./templates/blog_past_post_base.html")
             thought_content = write_thoughts_content(base_html,blog_posts,blog_past_posts)
             #write 'content' for Thoughts main page
-            complete_page = placeholder_replacement_base(base,page['title'],thought_content)
+            complete_page = placeholder_replacement_base(base,file_title,thought_content,pages)
         else:
-            complete_page = placeholder_replacement_base(base,page['title'],base_html)
+            complete_page = placeholder_replacement_base(base,file_title,base_html,pages)
         write_pages(complete_page,file_output)
     
 
     print('Site complete! Please review for accuracy.')
- 
-if __name__ == "__main__":
-    main()
-
+ ###########################################################################################
